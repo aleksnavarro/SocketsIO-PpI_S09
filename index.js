cd6a819e6340bd2e23f5a6bb1f5c7d99c0d43abb
aleksnavarro <jairo.navarros@alumnos.udg.mx>
2019-03-08 09:20:07 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//Setup de app
+var app = express();
+var server = app.listen(4000,function(){
+  console.log('escuchando peticiones en el puerto 4000')
+});
+
+// Archivos estaticos
+app.use(express.static('public'));
+
+// Socket Setup
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('conexion socket exitosa', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
