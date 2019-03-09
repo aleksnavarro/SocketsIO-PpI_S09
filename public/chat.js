fe2b8c81d89ea3857daf077c81ebe0e70df8f3a8
aleksnavarro <jairo.navarros@alumnos.udg.mx>
2019-03-09 10:27:20 -0600

    cambios para heroku

--- public/chat.js
+++ public/chat.js
@@ -19,7 +19,7 @@ btn.addEventListener('click',function(){
 });
 
 message.addEventListener('keypress',function(){
-  socket.emit('typing'.handle.value);
+  socket.emit('typing',handle.value);
 });
 
 // Escuchar Eventos
@@ -28,6 +28,6 @@ socket.on('chat',function(data){
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 
-socket.on('typing',function(data){
-    feedback.innerHTML = '<p><em>' + data + ' esta escribiendo...</em></p>';
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
