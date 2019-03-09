9e307e018dd74a93d103d4bbc889e64ec35ec06c
aleksnavarro <jairo.navarros@alumnos.udg.mx>
2019-03-09 09:56:26 -0600

    perfil para heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var socket = require('socket.io');
 
-//Setup de app
+// Setup de app
 var app = express();
 var server = app.listen(4000,function(){
   console.log('escuchando peticiones en el puerto 4000')
@@ -12,19 +12,18 @@ app.use(express.static('public'));
 
 // Socket Setup
 var io = socket(server);
-io.on('connection', (socket) => {
+io.on('connection',(socket)=>{
 
     console.log('conexion socket exitosa', socket.id);
 
-    // Handle chat event
+    // Evento de chat
     socket.on('chat', function(data){
         // console.log(data);
         io.sockets.emit('chat', data);
     });
 
-    // Handle typing event
+    // Evento de escritura
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
     });
-
 });
